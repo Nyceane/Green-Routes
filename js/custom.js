02c91324479f5957877c43b039947f13b2e8d35a
jihye13621 <jihye13621@yahoo.com>
2020-02-01 15:59:44 -0800

    slider change 2 values

--- js/custom.js
+++ js/custom.js
@@ -11,5 +11,18 @@ function setUpSlider() {
 
     slider.oninput = function() {
         output.innerHTML = this.value;
+        switch (output.innerHTML) {
+            case '1':
+                $('.card-title-carbon').text('Gas');
+                $('.card-title-cost').text('EV');
+                break;
+            case '2':
+                $('.card-title-carbon').text('Gas!!!');
+                $('.card-title-cost').text('EV!!!');
+                break;
+            default:
+                console.log('other num');
+                break;
+        }
     }
 }
